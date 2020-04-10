3e15bf54d13266a90e57191e81d700bf3c17d36e
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-10 14:38:27 +0300

    Exercise 8 Init!

---
+++ Exercise8/scripts.js
@@ -0,0 +1,66 @@
+function fontSize(size) {
+    var imgSmall = document.getElementById("small_letters");
+    var imgNormal = document.getElementById("normal_letters");
+    var imgBig = document.getElementById("big_letters");
+    var p = document.querySelectorAll("p");
+    var h1 = document.querySelectorAll("h1");
+    var h2 = document.querySelectorAll("h2");
+    if (size === "small") {
+        src = "letters/letterAsmallActive.png";
+        imgSmall.src = src;
+        imgNormal.src = "letters/letterA.png";
+        imgBig.src = "letters/letterAbig.png";
+
+        document.querySelector("body").style.fontSize = "10px";
+        for (i = 0; i < p.length; i++) {
+            p[i].style.fontSize = "10px";
+        }
+
+        for (i = 0; i < h1.length; i++) {
+            h1[i].style.fontSize = "22px";
+        }
+
+        for (i = 0; i < h2.length; i++) {
+            h2[i].style.fontSize = "16px";
+        }
+
+    } else if (size === "normal") {
+        src = "letters/letterAActive.png";
+        imgNormal.src = src;
+        imgSmall.src = "letters/letterAsmall.png";
+        imgBig.src = "letters/letterAbig.png";
+
+        document.querySelector("body").style.fontSize = "16px";
+        for (i = 0; i < p.length; i++) {
+            p[i].style.fontSize = "16px";
+        }
+
+        for (i = 0; i < h1.length; i++) {
+            h1[i].style.fontSize = "32px";
+        }
+
+        for (i = 0; i < h2.length; i++) {
+            h2[i].style.fontSize = "24px";
+        }
+
+    } else {
+        src = "letters/letterAbigActive.png";
+        imgBig.src = src;
+        imgSmall.src = "letters/letterAsmall.png";
+        imgNormal.src = "letters/letterA.png";
+
+        document.querySelector("body").style.fontSize = "20px";
+        for (i = 0; i < p.length; i++) {
+            p[i].style.fontSize = "20px";
+        }
+
+        for (i = 0; i < h1.length; i++) {
+            h1[i].style.fontSize = "50px";
+        }
+
+        for (i = 0; i < h2.length; i++) {
+            h2[i].style.fontSize = "44px";
+        }
+
+    }
+}
